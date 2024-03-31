4049935376dbbec245d2d71ca42766f39b66d4e9
Xatra1 <124910045+Xatra1@users.noreply.github.com>
2024-03-31 00:03:56 -0400

    Revert back to debugger state

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var debugConsole = document.getElementById('debugconsole').textContent,
 sysCheck();
 
 function sysCheck() {
-  let userAgent = navigator.userAgent,
+  /*let userAgent = navigator.userAgent,
     userAgentData = navigator.userAgentData,
     browsers = ['MSIE', 'Firefox', 'Safari', 'Chrome', 'OPR', 'Edg'],
     oses = ['X11', 'Windows', 'Mac', 'Linux'],
@@ -41,9 +41,9 @@ function sysCheck() {
   if (userAgentData != undefined) { os = userAgentData.platform; browserStr = `${userAgentData.brands[brandIndex].brand} v${userAgentData.brands[brandIndex].version}`; } else if (userAgent.includes('PlayStation')) os = 'PlayStation'; else { while (osIndex > -1 && userAgent.indexOf(oses[osIndex]) == -1) osIndex--; if (osIndex > -1) os = oses[osIndex]; if (os == 'X11') os = 'Unix'; }
   if (browserStr == 'Edg') browserStr = 'Edge'; else if (browserStr == 'OPR') browserStr = 'Opera';
   if (url == 'https://coin-clicker.surge.sh/') url = 'Surge'; else if (url == 'http://coinclicker.cc/') url = 'Dev Webserver'; else if (window.location.pathname.includes('index.html')) url = 'Local File';
-  runningBrowserString.textContent = `${browserStr} on ${os} saying hello from ${url}`;
-  /*runningBrowserString.textContent = navigator.userAgent;
-  document.getElementById('skipintrostring').textContent = supportsNavData();*/
+  runningBrowserString.textContent = `${browserStr} on ${os} saying hello from ${url}`;*/
+  runningBrowserString.textContent = navigator.userAgent;
+  document.getElementById('skipintrostring').textContent = supportsNavData();
   titleScreen.style.display = 'block';
 }
 
